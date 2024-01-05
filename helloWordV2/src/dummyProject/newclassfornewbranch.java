6dcaa59e8afe801f050c88388cc308caa59a4d88
bingivs <bingivs@gmail.com>
2024-01-05 17:56:31 +0530

    2nd commit

---
+++ helloWordV2/src/dummyProject/newclassfornewbranch.java
@@ -0,0 +1,10 @@
+package dummyProject;
+
+public class newclassfornewbranch {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("new class for new branch");
+	}
+
+}
